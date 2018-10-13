85d23a040d1b9db60da1cc87a05c1bf57a83aebf
pepo9706 <44114567+pepo9706@users.noreply.github.com>
2018-10-13 14:23:00 +0300

    Add files via upload

---
+++ dcbot.py
@@ -0,0 +1,33 @@
+import discord 
+from discord.ext import commands
+import time
+
+bot = commands.Bot(command_prefix ='#')
+
+@bot.event
+async def on_message(message):
+    if message.author==bot.user:
+        return
+    
+    
+    
+@bot.event
+async def on_member_join(member):
+    name = member.name
+    if(name.find("discord.gg/") >-1):
+        print("contains")
+        await bot.ban(member,delete_message_days=6)
+        time.sleep(2)
+        await bot.purge_from(bot.get_channel('360089937611325441'), limit=2)
+        await client.send_message(bot.get_channel('360089937611325441'), "Spoko prioteli nqkav random gei iskashe da reklamira server no az vi protectnah ot tozi gei")
+
+
+
+@bot.event
+async def on_ready():
+    print("logged in as")
+    print(bot.user.name)
+
+
+
+bot.run("NTAwNTkwNzk1NzYyNTY1MTI2.DqNDYw.tgJzIPfOAF72JYVK3YiYjBxIwb0")

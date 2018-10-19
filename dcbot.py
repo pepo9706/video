913332f815caf597032fbda1cc09bccb32a574a7
pepo9706 <44114567+pepo9706@users.noreply.github.com>
2018-10-19 12:27:45 +0300

    Update dcbot.py

--- dcbot.py
+++ dcbot.py
@@ -17,13 +17,15 @@ async def on_member_join(member):
     name = member.name
     if(name.find("discord.gg/") >-1):
         print("contains")
+        await bot.send_message(member, 'Gori v moita banq retard umrql , dano da hvanesh rak i da umresh v kalta ,pedal qj kur')
+        time.sleep(1)
         await bot.ban(member,delete_message_days=6)
         print(name+" was banned")
-        time.sleep(5)
+        time.sleep(3)
         await bot.purge_from(bot.get_channel('360089937611325441'), limit=2)
         time.sleep(3)
         await bot.send_message(bot.get_channel('360089937611325441'), "Spoko prioteli nqkav random gei iskashe da reklamira server no az vi protectnah ot tozi gei")
-        await bot.send_message(member, 'Gori v moita banq retard umrql , dano da hvanesh rak i da umresh v kalta ,pedal qj kur')
+        
 
 
 @bot.event

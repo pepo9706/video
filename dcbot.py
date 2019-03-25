7fd2ec8ddd1247d2ef67d295bce6eaa2f6b8e6af
pepo9706 <44114567+pepo9706@users.noreply.github.com>
2019-03-25 23:21:25 +0200

    Update dcbot.py

--- dcbot.py
+++ dcbot.py
@@ -13,7 +13,7 @@ async def on_message(message):
         await bot.purge_from(bot.get_channel('323779754513924106'), limit=1)
         time.sleep(1)
         await bot.send_message(message.channel, message.author.mention+(" shte te vkaram v banq :) laino"))
-        role = bot.get(message.server.roles, name='Silenced')
+        role = discord.utils.get(message.server.roles, name='Silenced')
         await bot.add_roles(message.author, role)
     
     
